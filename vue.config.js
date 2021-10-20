319597ed203df2dd08a32ecec5ab2438c09db3b8
Patrique Legault <patrique.legault@gmail.com>
2021-10-19 21:52:36 -0400

    Public path

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 module.exports = {
     outputDir: 'docs',
+    publicPath: '/io.github.vm.patlego.edu/',
     pages: {
         index: {
             entry: 'src/main.js',
-            title: 'Rockland Tutors'
-        }
+            title: 'Rockland Tutors',
+        },
     },
 }
